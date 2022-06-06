7346508f82e922de99314531c77568a1224ac59e
felix fedronic <fedronicfelix@gmail.com>
2022-06-06 22:10:42 +0200

    rendering components from markdown

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,16 +1,34 @@
 import classes from "./post-content.module.css";
 import PostHeader from "./post-header";
 import ReactMarkdown from "react-markdown";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { useState } from "react"
 
-function postContent(props) {
+function PostContent(props) {
   const { post } = props;
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+  const codeComponent = {
+    code(code) {
+      let { className, children } = code;
+      const result = className.match("js");
+      if (result) {
+        className = "javascript";
+        return (
+          <SyntaxHighlighter language={className} style={atomDark}>
+            {children}
+          </SyntaxHighlighter>
+        );
+      }
+    },
+  };
+
   return (
     <article className={classes.content}>
       <PostHeader image={imagePath} title={post.title} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={codeComponent}>{post.content}</ReactMarkdown>
     </article>
   );
 }
 
-export default postContent;
+export default PostContent;
